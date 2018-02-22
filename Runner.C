f4fb7938152ce683fcf9cf332dee6915da3ec006
Audrey Meadows <s592714@sbuniv.edu>
2018-02-22 02:22:47 +0000

    I cleaned up my code and made it so it has a chance of running. WILL NOT PRINT

--- Runner.C
+++ Runner.C
@@ -2,55 +2,58 @@
 
 int main(void)
 {
-  int TO = 0;
-  int FROM = 0;
-  int visited = 1;
-  int node = 0;// will need adjusting
+/*                                                             */
+/****************Array and Variable Declarations****************/
+/*                                                             */
+  int start = 0;
+  int smallCost;
+  int smallCostPos
+  const int totalNodes = 4;
 
-  //For clarity, rows are FROM and columns are TO
+  int visited[totalNodes] = {1,0,0,0}; //0 = false, 1 = true
+  int cost[totalNodes] = {INT_MAX, INT_MAX, INT_MAX, INT_MAX};
+  int previous[totalNodes] = {NULL, NULL, NULL, NULL};
   int adjacencyMatrix [totalNodes][totalNodes] = {{0,3,7,9},
                                                   {3,0,2,4},
                                                   {7,2,0,1},
                                                   {9,4,1,0}};
-  int visitedArr [totalNodes] = {1,0,0,0}; //0 = false, 1 = true
-  int cost [totalNodes] = {INT_MAX, INT_MAX, INT_MAX, INT_MAX};
-  char previous [totalNodes] = {NULL, NULL, NULL, NULL};
-
-  int nodeCount = 0; //Dynamic internal loop variable
-  const int totalNodes = 4;
-
-  while(visited<totalNodes)
+/*                                                             */
+/******************Executable Code Begins Here******************/
+/*                                                             */
+  for(visitNum = 1; visitNum <= totalNodes; visitNum++)
   {
-    while(nodeCount <= totalNodes)
+/******************Find the cheapest path*****************/
+
+    for(end = 0; end < totalNodes; end++)
     {
-      if(visited[FROM] == 0 && cost[FROM] != 0) //is this TO node visited? is this TO node the FROM node?
+      if(visited[end] == 0 && start != end)
       {
-        if(adjacencyMatrix[TO][FROM] < cost[FROM])// is the current cost of the node > the cost of the node from
+        if(adjacencyMatrix[start][end] < cost[start])
         {
-          //update cost[] and previosu of TO node
-          cost[TO] = adjacencyMatrix[TO][FROM];
-          previous[TO] = FROM;
+          cost[end] = adjacencyMatrix[start][end];
+          previous[end] = start;
         }
       }
-    TO++;
-    }//barginCosts
+    }
 
-    nodeCount = 0;
-    int lowestCost;
-    int lowestCostPos;
+/*************Find the next inexpensive start*************/
 
-    while(nodeCount < totalNodes)
+    smallCost = INT_MAX;
+    smallCostPos = -1;
+
+    for(node = 0; node < (totalNodes - visitNum); node++)
+   // while(nodeCount < (totalNodes - visited))
     {
-      if(visitedArr[node] == 0 && currentCost < lowestCost)
+      if(visited[node] == 0 && cost[node] < smallCost)
       {
-        lowestCost = currentCost;
-        lowestCostPos = node;                                           //  currentCost needs to be accessed via the cost array
+        smallCost = cost[node];
+        smallCostPos = node;
       }
-    }//find next FROM node
+    }
 
-    visited++;
-    nodeCount = 0;
-  }//mamaWhileLoop
-
+    start = smallCostPos;
+    visited[start] = 1; //sets the new start node as visited
+  }
+//print to show that I'm a winner
 
 }//main
